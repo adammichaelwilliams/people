b7cb13bc0040a3b9f7006ac18f17f231b7eaa554
Adam Williams <kainolophobia@gmail.com>
2014-07-06 15:08:50 -0400

    Not sure what's changed

--- models/person.js
+++ models/person.js
@@ -1,6 +1,6 @@
 
 var neo4j = require("neo4j");
-var db = new neo4j.GraphDatabase('http://localhost:7474');
+var db = new neo4j.GraphDatabase('http://localhost:7447');
 
 var Skill = require('./skill');
 
